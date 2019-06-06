4299812871a7d647f680985c5c9f8232a2e4ef98
Ralph de Horde <ralph.de.horde@hva.nl>
2019-06-07 01:39:36 +0200

    Added the rest

--- Movie/app/src/main/java/com/example/movie/MainActivity.java
+++ Movie/app/src/main/java/com/example/movie/MainActivity.java
@@ -1,50 +1,117 @@
 package com.example.movie;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.movie.MovieAPI.MovieRepository;
+import com.example.movie.MovieDB.Movie;
+import com.example.movie.View.MainActivityViewModel;
+import com.example.movie.View.MovieListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 	
-	private Button mButton;
-	private EditText mEditText;
 	
+	private MovieListAdapter movieListAdapter;
 	private RecyclerView mRecyclerView;
-	
-	private MovieRepository mMovieRepository;
-	
+	private List<Movie> movieList = new ArrayList<>();
+	private ImageView posterImage;
+	private Button btnMovies;
+	private EditText yearEt;
+	private MainActivityViewModel viewModel;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		initButton();
-		initText();
-		
-		
-		
+		initViews();
+		initObservers();
+		initRecyclerView();
 	}
 	
-	private void initButton() {
-		mButton = findViewById(R.id.submit);
-		mButton.setOnClickListener(new View.OnClickListener() {
+	private void initRecyclerView() {
+		movieListAdapter = new MovieListAdapter(this, movieList);
+		mRecyclerView = findViewById(R.id.rvMovieList);
+		mRecyclerView.setAdapter(movieListAdapter);
+		final GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 1, LinearLayoutManager.VERTICAL, false);
+		mRecyclerView.setLayoutManager(gridLayoutManager);
+		mRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(
+				new ViewTreeObserver.OnGlobalLayoutListener() { // Add Global Layout Listener to calculate the span count.
+					@Override
+					public void onGlobalLayout() {
+						mRecyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+						gridLayoutManager.setSpanCount(calculateSpanCount());
+						gridLayoutManager.requestLayout();
+					}
+				});
+		viewModel.getMovies();
+	}
+	
+	/**
+	 * Calculate the number of spans for the recycler view based on the recycler view width.
+	 *
+	 * @return int number of spans.
+	 */
+	private int calculateSpanCount() {
+		int viewWidth = mRecyclerView.getMeasuredWidth();
+		float cardViewWidth = getResources().getDimension(R.dimen.poster_width);
+		float cardViewMargin = getResources().getDimension(R.dimen.margin_medium);
+		int spanCount = (int) Math.floor(viewWidth / (cardViewWidth + cardViewMargin));
+		return spanCount >= 1 ? spanCount : 1;
+	}
+	
+	private void initViews() {
+		posterImage = findViewById(R.id.posterImage);
+		btnMovies = findViewById(R.id.submitBtn);
+		yearEt = findViewById(R.id.yearTextInput);
+		btnMovies.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				
+				viewModel.getMovieYear(yearEt.getText().toString());
+			}
+		});
+	}
+	private void initObservers() {
+		viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+		viewModel.getError().observe(this, new Observer<String>() {
+			@Override
+			public void onChanged(@Nullable String s) {
+				showToast(s);
+			}
+		});
+		viewModel.getMovie().observe(this, new Observer<List<Movie>>() {
+			@Override
+			public void onChanged(@Nullable List<Movie> movies) {
+				movieList = movies;
+				updateUI();
 			}
 		});
 	}
 	
-	private void initText() {
-		mEditText = findViewById(R.id.editYear);
+	private void updateUI() {
+		if (movieListAdapter == null) {
+			movieListAdapter = new MovieListAdapter(this, movieList);
+			mRecyclerView.setAdapter(movieListAdapter);
+		} else {
+			movieListAdapter.swapList(movieList);
+		}
 	}
 	
-	private void initRecyclerView() {
-		mRecyclerView = findViewById(R.id.recyclerView);
+	private void showToast(String message) {
+		Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG)
+				.show();
 	}
 }
--- Movie/app/src/main/java/com/example/movie/View/MainActivityViewModel.java
+++ Movie/app/src/main/java/com/example/movie/View/MainActivityViewModel.java
@@ -2,11 +2,47 @@ package com.example.movie.View;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
+import android.support.v7.util.SortedList;
+
+import com.example.movie.MovieAPI.MovieRepository;
+import com.example.movie.MovieDB.Movie;
+import com.example.movie.MovieDB.Result;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivityViewModel extends AndroidViewModel {
 	
+	private MovieRepository movieRepository = new MovieRepository();
+	
+	private MutableLiveData<Movie> movie = new MutableLiveData<>();
+	private MutableLiveData<String> error = new MutableLiveData<>();
+	
 	public MainActivityViewModel(@NonNull Application application) {
 		super(application);
+	}
+	
+	public void getMovie() {
+		movieRepository
+				.getMovie()
+				.enqueue(new Callback<Movie>() {
+					
+					@Override
+					public void onResponse(@NonNull Call<Movie> call, @NonNull Response<Movie> response) {
+						if (response.isSuccessful() && response.body() != null) {
+							movie.setValue(response.body());
+						} else {
+							error.setValue("Api Error: " + response.message());
+						}
+					}
+					
+					@Override
+					public void onFailure(@NonNull Call<Movie> call, @NonNull Throwable t) {
+						error.setValue("Api Error: " + t.getMessage());
+					}
+				});
 	}
 }
